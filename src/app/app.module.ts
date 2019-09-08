fe7ef9d9d48e0908909feba2809c2a4897f88650
DraganJankovic997 <gagibratee@gmail.com>
2019-09-08 18:00:43 +0200

    Services and models implemented, components created with generate component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { AutorComponent } from './components/autor/autor.component';
+import { IgracComponent } from './components/igrac/igrac.component';
+import { LigaComponent } from './components/liga/liga.component';
+import { NacionalnostComponent } from './components/nacionalnost/nacionalnost.component';
+import { TimComponent } from './components/tim/tim.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    AutorComponent,
+    IgracComponent,
+    LigaComponent,
+    NacionalnostComponent,
+    TimComponent
   ],
   imports: [
     BrowserModule
